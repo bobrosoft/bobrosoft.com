6d2c91721c80e9192d3797fd1ec142e98baa81c9
bobrosoft <bobrosoft@yandex.ru>
2022-08-17 21:49:32 +0200

    CARD-4: [App] Add eslint

--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './styles/styles.scss';
 import App from './pages/App/App';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
--- src/pages/App/App.tsx
+++ src/pages/App/App.tsx
@@ -3,7 +3,6 @@ import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import {MainPage} from '../MainPage/MainPage';
 import {StyledApp} from './StyledApp';
 
-
 const App: React.FC = () => {
   return (
     <StyledApp>
