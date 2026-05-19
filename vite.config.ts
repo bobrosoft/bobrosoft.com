6555d83986f14263d8f8101d194daa07ac2e5ccd
bobrosoft <bobrosoft@yandex.ru>
2026-05-20 01:22:55 +0400

    chore: switch to Vite

---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+import {defineConfig} from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  base: '/',
+  build: {
+    outDir: 'build',
+    sourcemap: false,
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        api: 'modern-compiler',
+      },
+    },
+  },
+});
+
